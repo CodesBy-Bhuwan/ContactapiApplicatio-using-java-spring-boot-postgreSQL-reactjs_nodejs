aa6669f4bde231750722464c62c6417d1547f6f3
COdesBy-Bhuwan <bhuwan.thadarai817@gmail.com>
2024-01-22 21:11:17 +0545

    toggleModel && numberofContacts

---
+++ contactfe/src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+// instead of using props magic technique we can use inside of props variable directly e.g.: toggleModel
+const Header = ({ toggleModel, nbOfContacts }) => {
+  return (
+    <div>
+      <header className='header'>
+        <div className='container'>
+            <h3>Contact List ({nbOfContacts})</h3>
+            <button className='btn' onClick={()=> toggleModel(true)}>
+                <i className='bi bi-plus-quare'></i> Add New Contact</button>
+        </div>
+      </header>
+    </div>
+  )
+}
+
+export default Header
